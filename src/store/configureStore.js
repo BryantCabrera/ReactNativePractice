55b267f73ac03ea7fba1fd9f310f0afdfe57393e
Bryant <cabrera.bryant@gmail.com>
2019-04-26 21:31:34 -0700

    Bryant connects IonIcons.

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, compose } from 'redux';
 
 import placesReducer from './reducers/places';
     
@@ -6,8 +6,19 @@ const rootReducer = combineReducers({
     places: placesReducer
 });
 
+let composeEnhancers = compose;
+
+//__DEV__ is provided by react native that is only exposed if you're in development mode
+if (__DEV__) {
+    composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+}
+
 const configureStore = () => {
-    return createStore(rootReducer);
+    // return createStore(rootReducer);
+
+    // With connection to redux developer tools
+    // We can pass apply middleware to composeEnhancers() because redux dev tools are not a middleware
+    return createStore(rootReducer, composeEnhancers());
 }
 
 export default configureStore;
