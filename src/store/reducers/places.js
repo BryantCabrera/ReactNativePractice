0bc98f265dbe8cccc158b7c00e44e34fcb59c669
Bryant <cabrera.bryant@gmail.com>
2019-04-26 17:07:44 -0700

    Bryant updates places.js redux reducer.

--- src/store/reducers/places.js
+++ src/store/reducers/places.js
@@ -1,3 +1,5 @@
+import { ADD_PLACE, DELETE_PLACE, SELECT_PLACE, DESELECT_PLACE } from '../actions/actionTypes';
+
 const intiialState = {
     places: [],
     selectedPlace: null
@@ -7,6 +9,37 @@ const intiialState = {
 //ES6 lets you set parameter to a default value
 const reducer = (state = initialState, action) => {
     switch (action.type) {
+        case ADD_PLACE:
+            return {
+                ...state,
+                places: prevState.places.concat({
+                    key: Math.random().toString(),
+                    name: action.placeName,
+                    image: {
+                        uri: "https://i.imgur.com/KbicDVh.jpg"
+                    }
+                })
+            };
+        case DELETE_PLACE:
+            return {
+                ...state,
+                places: prevState.places.filter(place => {
+                    return place.key !== state.selectedPlace.key;
+                }),
+                selectedPlace: null
+            };
+        case SELECT_PLACE:
+            return {
+                ...state,
+                selectedPlace: state.places.find(place => {
+                    return place.key === action.placeKey;
+                })
+            };
+        case DELETE_PLACE:
+            return {
+                ...state,
+                selectedPlace: null
+            };
         default:
             return state;
     }
