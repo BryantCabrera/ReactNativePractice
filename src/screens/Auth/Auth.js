6aa5f3f049a8485a446aa2ec35c733115f832765
Bryant <cabrera.bryant@gmail.com>
2019-04-28 22:08:40 -0700

    Bryant adds image background to Auth.js.

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
+import { View, Text, Button, TextInput, StyleSheet, ImageBackground } from 'react-native';
 
 import startMainTabs from '../MainTabs/startMainTabs';
 import DefaultInput from "../../components/UI/DefaultInput/DefaultInput";
 import HeadingText from "../../components/UI/HeadingText/HeadingText";
 import MainText from "../../components/UI/MainText/MainText";
 import ButtonWithBackground from "../../components/UI/ButtonWithBackground/ButtonWithBackground";
+import backgroundImage from "../../assets/background.jpg";
 
 class AuthScreen extends Component {
     loginHandler = () => {
@@ -14,26 +15,28 @@ class AuthScreen extends Component {
 
     render() {
         return (
-            <View>
-                {/* Before creating HeadingText.js component <Text>Auth Screen</Text> */}
-                <MainText>
-                    <HeadingText>Please Log In</HeadingText>
-                </MainText>
-                <Button title="Login" onPress={this.loginHandler} />
-                <View style={styles.inputContainer}>
-                    {/* Before modulatizing components <TextInput placeholder="Your E-Mail Address" tyle={styles.input} />
-                    <TextInput placeholder="Password" tyle={styles.input} />
-                    <TextInput placeholder="Confirm Password" tyle={styles.input} /> */}
+            <ImageBackground source={backgroundImage} style={styles.backgroundImage}>
+                <View>
+                    {/* Before creating HeadingText.js component <Text>Auth Screen</Text> */}
+                    <MainText>
+                        <HeadingText>Please Log In</HeadingText>
+                    </MainText>
+                    <Button title="Login" onPress={this.loginHandler} />
+                    <View style={styles.inputContainer}>
+                        {/* Before modulatizing components <TextInput placeholder="Your E-Mail Address" tyle={styles.input} />
+                        <TextInput placeholder="Password" tyle={styles.input} />
+                        <TextInput placeholder="Confirm Password" tyle={styles.input} /> */}
 
-                    <DefaultInput
-                        placeholder="Your E-Mail Address"
-                        style={styles.input}
-                    />
-                    <DefaultInput placeholder="Password" style={styles.input} />
-                    <DefaultInput placeholder="Confirm Password" style={styles.input} />
+                        <DefaultInput
+                            placeholder="Your E-Mail Address"
+                            style={styles.input}
+                        />
+                        <DefaultInput placeholder="Password" style={styles.input} />
+                        <DefaultInput placeholder="Confirm Password" style={styles.input} />
+                    </View>
+                    <Button title="Submit" onPress={this.loginHandler} />
                 </View>
-                <Button title="Submit" onPress={this.loginHandler} />
-            </View>
+            </ImageBackground>
         );
     }
 }
@@ -43,6 +46,10 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: "center",
         alignItems: "center"
+    },
+    backgroundImage: {
+        width: "100%",
+        flex: 1
     },
     inputContainer: {
         width: "80%"
