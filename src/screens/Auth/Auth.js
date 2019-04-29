895dbf37771188fa1bbdd0309b4d77fc1d4bd857
Bryant <cabrera.bryant@gmail.com>
2019-04-28 21:35:05 -0700

    Bryant adds input styling and creates DefaultInput.js component.

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
 
 import startMainTabs from '../MainTabs/startMainTabs';
+import DefaultInput from "../../components/UI/DefaultInput/DefaultInput";
+import HeadingText from "../../components/UI/HeadingText/HeadingText";
+import MainText from "../../components/UI/MainText/MainText";
+import ButtonWithBackground from "../../components/UI/ButtonWithBackground/ButtonWithBackground";
 
 class AuthScreen extends Component {
     loginHandler = () => {
@@ -13,9 +17,37 @@ class AuthScreen extends Component {
             <View>
                 <Text>Auth Screen</Text>
                 <Button title="Login" onPress={this.loginHandler} />
+                <View style={styles.inputContainer}>
+                    {/* Before modulatizing components <TextInput placeholder="Your E-Mail Address" tyle={styles.input} />
+                    <TextInput placeholder="Password" tyle={styles.input} />
+                    <TextInput placeholder="Confirm Password" tyle={styles.input} /> */}
+
+                    <DefaultInput
+                        placeholder="Your E-Mail Address"
+                        style={styles.input}
+                    />
+                    <DefaultInput placeholder="Password" style={styles.input} />
+                    <DefaultInput placeholder="Confirm Password" style={styles.input} />
+                </View>
+                <Button title="Submit" onPress={this.loginHandler} />
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    inputContainer: {
+        width: "80%"
+    },
+    input: {
+        backgroundColor: "#eee",
+        borderColor: "#bbb"
+    }
+});
 
 export default AuthScreen;
