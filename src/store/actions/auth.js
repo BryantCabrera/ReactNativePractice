fbc33f5d9d8d705bb0c82c6f2a3b673c4248b19b
Bryant <cabrera.bryant@gmail.com>
2019-05-01 23:46:14 -0700

    Bryant sets up firebase sign up in auth.js action.

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,8 +1,42 @@
 import { TRY_AUTH } from './actionTypes';
 
 export const tryAuth = (authData) => {
-    return {
-        type: TRY_AUTH,
-        authData: authData
+    // Replaced in Module 11: Auth
+    // return {
+    //     type: TRY_AUTH,
+    //     authData: authData
+    // };
+
+    // For Module 11: Auth
+    return dispatch => {
+        dispatch(authSignup(authData));
+    };
+};
+
+export const authSignup = (authData) => {
+    return dispatch => {
+        // to find your key: firebase console > gear > API Key
+            //  OLD WAY: firebase console > authentication > web setup in top right > copy apiKey
+        fetch("https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyDzuG1WWDn3Sbfva0eqVmBXcu49HJojOCA", {
+            method: "POST",
+            body: JSON.stringify({
+                email: authData.email,
+                password: authData.password,
+                returnSecureToken: true
+            }),
+            // also need headers for Firebase
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .catch(err => {
+                console.log(err);
+                alert("Authentication failed, please try again!");
+            })
+            .then(res => res.json())
+            .then(parsedRes => {
+                // need to make sure to handle errors responses, like 4xx and 5xx
+                console.log(parsedRes);
+            });
     };
 };
