6d7d991455651d44261242bf7f48e0d8c237671c
Bryant <cabrera.bryant@gmail.com>
2019-05-02 01:05:54 -0700

    Bryant stores auth token in redux store.

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,4 +1,4 @@
-import { TRY_AUTH } from './actionTypes';
+import { TRY_AUTH, AUTH_SET_TOKEN } from './actionTypes';
 import { uiStartLoading, uiStopLoading } from "./index";
 import startMainTabs from "../../screens/MainTabs/startMainTabs";
 
@@ -50,9 +50,14 @@ export const tryAuth = (authData, authMode) => {
             .then(res => res.json())
             .then(parsedRes => {
                 dispatch(uiStopLoading());
-                if (parsedRes.error) {
+
+                // if (parsedRes.error) {
+                // throws alert if no idToken
+                if (!parsedRes.idToken) {
                     alert("Authentication failed, please try again!");
                 } else {
+                    dispatch(authSetToken(parsedRes.idToken));
+
                     startMainTabs();
                 }
             });
@@ -98,3 +103,10 @@ export const tryAuth = (authData, authMode) => {
 //             });
 //     };
 // };
+
+export const authSetToken = token => {
+    return {
+        type: AUTH_SET_TOKEN,
+        token: token
+    };
+};
