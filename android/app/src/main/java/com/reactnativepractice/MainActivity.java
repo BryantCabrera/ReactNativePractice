490e5c5964cdf9580754910498ccb7ae3374f17d
Bryant <cabrera.bryant@gmail.com>
2019-05-02 23:40:32 -0700

    Bryant creates Splash Screens for iOS and Android.

--- android/app/src/main/java/com/reactnativepractice/MainActivity.java
+++ android/app/src/main/java/com/reactnativepractice/MainActivity.java
@@ -3,6 +3,13 @@ package com.reactnativepractice;
 import com.facebook.react.ReactActivity;
 import com.reactnativenavigation.controllers.SplashActivity;
 
+// Added in Module 13: Publishing the App
+import android.widget.LinearLayout;
+import android.graphics.Color;
+import android.widget.TextView;
+import android.view.Gravity;
+import android.util.TypedValue;
+
 public class MainActivity extends ReactActivity {
 
     /**
@@ -13,4 +20,22 @@ public class MainActivity extends ReactActivity {
     // protected String getMainComponentName() {
     //     return "ReactNativePractice";
     // }
+
+    // Added in Module 13: Publishing the App
+    // we need t override a function that's already in ReactActivity
+    @Overridepublic LinearLayout createSplashLayout() {
+        LinearLayout view = new LinearLayout(this);
+        TextView textView = new TextView(this);
+
+        view.setBackgroundColor(Color.parseColor("#521751"));
+        view.setGravity(Gravity.CENTER);
+
+        textView.setTextColor(Color.parseColor("#fa923f"));
+        textView.setText("Awesome Places");
+        text.setGravity(Gravity.CENTER);
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 40);
+
+        view.addView(textView);
+        return view;
+    }
 }
